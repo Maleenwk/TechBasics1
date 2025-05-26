30e0c5b948112b469c05b5873903b194aa84d1dc
Maleenwk <Maleen.warnecke@stud.leuphana.de>
2025-05-26 09:00:55 +0200

    fixed ascii.py

---
+++ assignments/week1/ascii.py
@@ -0,0 +1,16 @@
+# ASCII art
+
+# --- bunny ---
+print(" (\\ _ /)")
+print(" ( 'x' )")
+print('c(\") (\")')
+
+
+# --- frog ---
+print("  @..@")
+print(" (----)")
+print("(>____<)")
+print("  M~~M")
+
+# --- just a guy ---
+print("¯\_(˙⸞˙)_/¯")
